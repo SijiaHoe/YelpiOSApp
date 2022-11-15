679f195c788019ccf7acfb088faa554f635345b8
SijiaHou <housj1228@gmail.com>
2022-11-15 11:05:27 -0800

    Initial Commit

---
+++ YelpReviewApp/YelpReviewAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  YelpReviewAppApp.swift
+//  YelpReviewApp
+//
+//  Created by LilHoe on 11/15/22.
+//
+
+import SwiftUI
+
+@main
+struct YelpReviewAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
