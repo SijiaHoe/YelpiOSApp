5ac8b696f88b78f8ad0b04dcc8bfe3ce6a8d8718
SijiaHou <housj1228@gmail.com>
2022-11-23 09:57:38 -0800

    UI of search section

--- YelpReviewApp/ContentView.swift
+++ YelpReviewApp/ContentView.swift
@@ -8,15 +8,87 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var keyword: String = ""
+    @State var distance: String = "10"
+    @State var categories: String = "all"
+    @State var location: String = ""
+    @State var checked: Bool = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+        NavigationView{
+            // search setion
+            VStack{
+                Form {
+                    Section{
+                        HStack {
+                            Text("Keyword:")
+                            TextField("Required", text: $keyword)
+                        }
+                        HStack {
+                            Text("Distance:")
+                            TextField("", text: $distance)
+                        }
+                        Picker(selection: $categories, label: Text("Category:")) {
+                            Text("Default").tag(1)
+                            Text("Arts and Entertainment").tag(2)
+                            Text("Health and Medical").tag(3)
+                            Text("Hotels and Travel").tag(4)
+                            Text("Food").tag(5)
+                            Text("Professional Services").tag(6)
+                        }
+                        HStack {
+                            Text("Location:")
+                            TextField("", text: $location)
+                        }
+                        Toggle(isOn: $checked) {
+                            Text("Auto-detect my location")
+                        }
+                        HStack(spacing: 30) {
+                            Button(action: {
+                                
+                            }) {
+                                Text("Submit")
+                                    .frame(width: 70 , height: 20, alignment: .center)
+                            }
+                            .foregroundColor(Color.white)
+                            .buttonStyle(.bordered)
+                            // todo: color change
+                            .background(Color.red)
+                            .controlSize(.large)
+                            .cornerRadius(10)
+                            
+                            Button(action: {
+                                
+                            }) {
+                                Text("Clear")
+                                    .frame(width: 70 , height: 20, alignment: .center)
+                            }
+                            .foregroundColor(Color.white)
+                            .buttonStyle(.bordered)
+                            .background(Color.blue)
+                            .controlSize(.large)
+                            .cornerRadius(10)
+                        }.frame(maxWidth: .infinity, alignment: .center)
+                    }
+                }
+            }
+            .navigationTitle("Business Search")
+            .toolbar {
+                Button(action:{
+                }) {
+                    Image(systemName: "calendar.badge.clock")
+                        .imageScale(.large)
+                        .foregroundColor(.accentColor)
+                }
+            }
+            
+            // search results section
+            VStack {
+                List {
+                    
+                }
+            }
+        } // Nav
+    } // body
 }
 
 struct ContentView_Previews: PreviewProvider {
